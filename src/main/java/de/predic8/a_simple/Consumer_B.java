7b9c16de385bcb695da4efa9c73c0eb75ff18e7d
Thomas Bayer <bayer@predic8.de>
2019-01-11 18:50:53 +0100

    Updated to Kafka 2.1.0

--- src/main/java/de/predic8/a_simple/Consumer_B.java
+++ src/main/java/de/predic8/a_simple/Consumer_B.java
@@ -18,7 +18,7 @@ public class Consumer_B {
 
         Properties props = new Properties();
         props.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(GROUP_ID_CONFIG, "b");
+        props.put(GROUP_ID_CONFIG, "notification");
         props.put(ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(SESSION_TIMEOUT_MS_CONFIG, "30000");
