0fe9b85b940bb01ecccd3380130a8cb9c679898b
Daniel Bonnauer <bonnauer@predic8.de>
2022-09-16 13:32:27 +0200

    filled in avro

--- src/main/java/de/predic8/n_avro/Schreiben.java
+++ src/main/java/de/predic8/n_avro/Schreiben.java
@@ -1,8 +1,25 @@
 package de.predic8.n_avro;
 
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Schreiben {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Article a = Article.newBuilder()
+                .setName("Beef")
+                .setPrice(5)
+                .setOrigin("Argentina")
+                .build();
 
+        DatumWriter<Article> userDatumWriter = new SpecificDatumWriter<>(Article.class);
+        DataFileWriter<Article> dataFileWriter = new DataFileWriter<>(userDatumWriter);
+        dataFileWriter.create(a.getSchema(), new File("article.avro"));
+        dataFileWriter.append(a);
+        dataFileWriter.close();
     }
 }
