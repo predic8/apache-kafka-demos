63ff9500bbebe60d99ad9185d7664daf31b4c8d6
thomas <bayer@predic8.de>
2026-04-28 09:54:22 +0200

    Migration auf 4.2.0

--- src/main/java/de/predic8/f_streams/SampleProcessor.java
+++ src/main/java/de/predic8/f_streams/SampleProcessor.java
@@ -1,18 +1,24 @@
 package de.predic8.f_streams;
 
-import org.apache.kafka.streams.processor.Processor;
-import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Record;
 
-public class SampleProcessor implements Processor<Integer,Double> {
+public class SampleProcessor implements Processor<Integer, Double, Void, Void> {
+
+    private ProcessorContext<Void, Void> context;
 
     @Override
-    public void init(ProcessorContext ctx) {
-        System.out.println("Init ctx = " + ctx);
+    public void init(ProcessorContext<Void, Void> context) {
+        this.context = context;
+        System.out.println("Init ctx = " + context);
     }
 
     @Override
-    public void process(Integer integer, Double aDouble) {
-        System.out.println("Processung integer = " + integer);
+    public void process(Record<Integer, Double> record) {
+        System.out.println("Processing key = " + record.key() +
+                ", value = " + record.value() +
+                ", timestamp = " + record.timestamp());
     }
 
     @Override
